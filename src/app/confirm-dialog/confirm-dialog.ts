a559ff112cfdf904a260e5136c56df53d21bd1e1
lpruiz <2023004864@ifam.edu.br>
2026-08-11 10:58:28 -0400

    add confirmation dialog before deleting a student

---
+++ src/app/confirm-dialog/confirm-dialog.ts
@@ -0,0 +1,22 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [],
+  templateUrl: './confirm-dialog.html',
+  styleUrl: './confirm-dialog.css',
+})
+export class ConfirmDialog {
+  message = input<string>('Tem certeza?');
+
+  confirmed = output<void>();
+  cancelled = output<void>();
+
+  onConfirm(): void {
+    this.confirmed.emit();
+  }
+
+  onCancel(): void {
+    this.cancelled.emit();
+  }
+}
